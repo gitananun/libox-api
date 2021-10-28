7b14e41f716bc70fa201f030cd8a028954e6476f
gitananun <tigran.muradyan140201@gmail.com>
2021-10-28 17:12:14 +0400

    Course | Model | Factory | Seeder | Controller | Migration

---
+++ database/migrations/2021_10_27_132225_create_courses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');
+            $table->float('rating');
+            $table->float('price');
+            $table->float('length');
+            $table->string('language');
+            $table->text('description');
+            $table->timestamp('last_updated');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,5 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+    {}
 }
